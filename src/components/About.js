075390c57e89ea9603d521925c53b98f79a1707e
kisublee <yigi2021@gmail.com>
2021-12-04 01:00:20 -0500

    create About component and add article tags for each members bio'
    q
    w

---
+++ src/components/About.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import '../css/About.css'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div className = "TBD">
+
+            <article>
+                <h2>Jose Bordon</h2>
+                <div></div>
+            </article>
+
+            <article>
+                <h2>Ki Sub Lee</h2>
+                <div></div>
+            </article>
+
+            <article>
+                <h2>Jalamang Janka</h2>
+                <div></div>
+            </article>
+
+      </div>
+    );
+  }
+}
