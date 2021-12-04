069354ab75df88bfdf88bb38844b43349a84a8b6
Jalamang <Jalamangjanka@gmail.com>
2021-12-03 22:30:52 -0500

    Added Nav component w/ some css effect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import './App.css';
+import React from "react";
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+
+/*
+  Components
+*/
+
+import Nav from "./components/Nav";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="App">Sup</div>;
+  return (
+    <div className="App">
+       
+    <Nav/>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
