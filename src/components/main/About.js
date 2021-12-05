205b2609e0d25a2c0faebb799068fb2afeb120cb
kisublee <yigi2021@gmail.com>
2021-12-05 11:06:23 -0500

    Delete the current About.js and move to the new one in main folder.

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import '../css/About.css'
 
 export default class About extends Component {
   render() {
-    return <main>About Us</main>;
+    return (
+      <main className = "TBD">
+
+            <article>
+                <h2>Jose Bordon</h2>
+                <div></div>
+            </article>
+
+            <article>
+                <h2>Ki Sub Lee</h2>
+                <div></div>
+            </article>
+
+            <article>
+                <h2>Jalamang Janka</h2>
+                <div></div>
+            </article>
+
+      </main>
+    );
   }
 }
