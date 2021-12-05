f7d143cc09f3eeded25d98cf762a5909fbcff200
JoseBorbon <joserborbon@gmail.com>
2021-12-05 00:38:14 -0500

    add imports and routing component

--- src/components/Routings.js
+++ src/components/Routings.js
@@ -1,12 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import React, { Component } from 'react';
-import Home from './Home';
+import Home from './main/Home';
+import About from './main/About';
 
 export default class Routings extends Component {
   render() {
     return (
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={<Home searchResults={this.props.searchResults} />}
+        />
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/videos/:id" element={<Media />} /> WORK IN PROGRESS -- Jose --*/}
       </Routes>
     );
   }
