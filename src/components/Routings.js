966b6d98add95ae00b4e672d3ad8a9468375919e
JoseBorbon <joserborbon@gmail.com>
2021-12-06 08:41:07 -0500

    refactor to functional component

--- src/components/Routings.js
+++ src/components/Routings.js
@@ -1,19 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
-import React, { Component } from 'react';
 import Home from './main/Home';
 import About from './main/About';
+import Media from './main/Media';
 
-export default class Routings extends Component {
-  render() {
-    return (
-      <Routes>
-        <Route
-          path="/"
-          element={<Home searchResults={this.props.searchResults} />}
-        />
-        <Route path="/about" element={<About />} />
-        {/* <Route path="/videos/:id" element={<Media />} /> WORK IN PROGRESS -- Jose --*/}
-      </Routes>
-    );
-  }
-}
+const Routings = ({ searchResults }) => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home searchResults={searchResults} />} />
+      <Route path="about" element={<About />} />
+      <Route path="videos/*" element={<Media />} />
+    </Routes>
+  );
+};
+
+export default Routings;
