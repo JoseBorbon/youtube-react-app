d1642896c743e2670caf9df082d2921cfa80c073
JoseBorbon <joserborbon@gmail.com>
2021-12-06 22:31:45 -0500

    transfer logic to child components for SOC

--- src/components/main/Media.js
+++ src/components/main/Media.js
@@ -1,50 +1,18 @@
-import YouTube from 'react-youtube';
-import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
+import Video from './media-children/Video';
+import Comments from './media-children/Comments';
 import './Media.css';
 
-const Media = ({ opts }) => {
-  const [videoInfo, setVideoInfo] = useState([]);
+const Media = () => {
   const ytVideoId = Object.values(useParams())[0];
-
-  useEffect(() => {
-    (async () => {
-      let response = await fetch(
-        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${ytVideoId}&key=${process.env.REACT_APP_API_KEY}`
-      );
-      response = await response.json();
-      setVideoInfo(response.items[0].snippet);
-    })();
-  }, [ytVideoId]);
-
-  const onReady = (evt) => {
-    evt.target.pauseVideo();
-  };
 
   return (
     <main>
-      <h3>{videoInfo.title}</h3>
-      <YouTube
-        containerClassName="Youtube-video"
-        videoId={ytVideoId}
-        opts={opts}
-        onReady={onReady}
-      />
-      <h4>{videoInfo.channelTitle}</h4>
+      <Video ytVideoId={ytVideoId} />
       <hr />
+      <Comments />
     </main>
   );
-};
-
-Media.defaultProps = {
-  opts: {
-    height: '400',
-    width: '400',
-    playerVars: {
-      autoplay: 0,
-      origin: 'http://localhost:3000',
-    },
-  },
 };
 
 export default Media;
