6b2d663b3e1ae5edf96ea2b6eccee7f0ef2ea69b
JoseBorbon <joserborbon@gmail.com>
2021-12-09 04:06:21 -0500

    passed currBGColor props

--- src/components/Routings.js
+++ src/components/Routings.js
@@ -3,12 +3,17 @@ import Home from './main/Home';
 import About from './main/About';
 import Media from './main/Media';
 
-const Routings = ({ searchResults }) => {
+const Routings = ({ searchResults, currBGColor }) => {
   return (
     <Routes>
-      <Route path="/" element={<Home searchResults={searchResults} />} />
-      <Route path="about" element={<About />} />
-      <Route path="videos/*" element={<Media />} />
+      <Route
+        path="/"
+        element={
+          <Home searchResults={searchResults} currBGColor={currBGColor} />
+        }
+      />
+      <Route path="about" element={<About currBGColor={currBGColor} />} />
+      <Route path="videos/*" element={<Media currBGColor={currBGColor} />} />
     </Routes>
   );
 };
