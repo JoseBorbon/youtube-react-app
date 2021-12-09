484cb9626f52261dd36dc9482b63de2110ec40c8
JoseBorbon <joserborbon@gmail.com>
2021-12-09 15:36:17 -0600

    import Color Palette component to ColorSwitch comp

--- src/components/common/nav-children/ColorSwitch.js
+++ src/components/common/nav-children/ColorSwitch.js
@@ -3,9 +3,11 @@ import ColorPalette from './ColorPalette';
 const ColorSwitch = ({
   isColorSwitchToggled,
   handleSetIsColorSwitchToggled,
+  currBGColor,
+  setCurrBGColor,
 }) => {
   return isColorSwitchToggled ? (
-    <ColorPalette />
+    <ColorPalette currBGColor={currBGColor} setCurrBGColor={setCurrBGColor} />
   ) : (
     <button
       style={{ height: '20px', width: '50px' }}
