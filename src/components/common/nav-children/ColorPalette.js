621cb142c5e4c643052bb63c8f56aa3e29eb3e35
JoseBorbon <joserborbon@gmail.com>
2021-12-09 15:35:12 -0600

    add ColorPalette component

--- src/components/common/nav-children/ColorPalette.js
+++ src/components/common/nav-children/ColorPalette.js
@@ -1,5 +1,29 @@
-const ColorPalette = () => {
-  return <div style={{ height: '20px', width: '80px' }}>NEW COLOR WOOT</div>;
+import { v4 as uuid } from 'uuid';
+import './ColorPalette.css';
+
+const ColorPalette = ({ setCurrBGColor }) => {
+  const handleClick = (e) => {
+    setCurrBGColor(e.target.id);
+  };
+  const colors = ['red', 'blue', 'green', 'orange', 'yellow'];
+
+  const colorBoxes = colors.map((color) => (
+    <li
+      key={uuid()}
+      className="ColorPalette-box"
+      id={color}
+      style={{ backgroundColor: color }}
+      onClick={handleClick}
+    ></li>
+  ));
+  return (
+    <ul
+      className="ColorPalette-palette"
+      style={{ height: '20px', width: '80px' }}
+    >
+      {colorBoxes}
+    </ul>
+  );
 };
 
 export default ColorPalette;
