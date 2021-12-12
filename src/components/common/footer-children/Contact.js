6caaeb40b14b9a35f8f741007834e27c59d89cb6
Jose Borbon <62197854+JoseBorbon@users.noreply.github.com>
2021-12-11 20:49:27 -1000

    changes

--- src/components/common/footer-children/Contact.js
+++ src/components/common/footer-children/Contact.js
@@ -1,89 +1,111 @@
-import "./Contact.css";
-import React from "react";
+import './Contact.css';
+import React from 'react';
+import { Button, TextField, NativeSelect, FormControl } from '@mui/material';
 
 const Contact = ({ currBGColor }) => {
-  const styles = {
-    width:"650px",
-     height:"410px",
-      style:"border:0",
-       allowfullscreen:"",
-       loading:"lazy"
-};
   return (
-    <main>
-      <div className="contact" style={{ backgroundColor: currBGColor }}>
-        <section> <h3>Get In Touch</h3>
-          <div class="showcase-form">
-           
-            <form  name="contact" method="POST" data-netlify="true">
-              
-              <label>
-                <input type="text" name="myName" placeholder="Name" required />
-              </label>
-              <label >
-                <input
-                  type="text"
-                  name="company"
-                  placeholder="Company Name"
-                  required
-                />
-              </label>
-              <div >
-                <input type="email" name="email" placeholder="Email" required />
-              </div>
-              <br />
-              <label>
-                Your Role : {""}
-                <select name="role[]" multiple>
-                  <option value="leader">Team Lead</option>
-                  <option value="follower">Full Stack Developer</option>
-                  <option value="follower">Front-End Developer</option>
-                  <option value="follower">Software Engineer</option>
-                </select>
-              </label>
-              <br />
-             
-              <div class="field">
-                <input type="file" name="myfile" placeholder="Upload file" />
-              </div>
-              
-              {/* spam filtering */}
-              <p class="field">
-                <div data-netlify-recaptcha="true"></div>
-              </p>
-              <p>
-                <label>
-                  Message : <textarea name="message"></textarea>
-                </label>
-              </p>
-              <br />
-              <input type="submit" value="Send" class="btn" />
-             
-            </form>
-          </div>
-        </section>
-       
-        <section>
-        
-          <div className="location card">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.8666592099808!2d-73.94411018451345!3d40.74295947932856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2592e9fcf1a65%3A0x7e37044e79db712a!2s47-10%20Austell%20Pl%202nd%20floor%2C%20Long%20Island%20City%2C%20NY%2011101!5e0!3m2!1sen!2sus!4v1639111475214!5m2!1sen!2sus" style={styles} ></iframe>
-          </div>
-          <div class="social">
-            <a href="#">
-              <i class="fab fa-github fa-2x"></i>
-            </a>
-            <a href="#">
-              <i class="fab fa-facebook fa-2x"></i>
-            </a>
-            <a href="#">
-              <i class="fab fa-instagram fa-2x"></i>
-            </a>
-            <a href="#">
-              <i class="fab fa-twitter fa-2x"></i>
-            </a>
-          </div>
-        </section>
-      </div>
+    <main className="contact" style={{ backgroundColor: currBGColor }}>
+      <FormControl className="showcase-form">
+        <h2>Get In Touch</h2>
+        <TextField
+          id="filled-name"
+          label="Name"
+          fullWidth
+          margin="normal"
+          variant="filled"
+          className="mui-input"
+          sx={{
+            margin: '0px',
+            marginTop: '5px',
+          }}
+        />
+        <TextField
+          id="filled-company"
+          label="Company"
+          fullWidth
+          margin="normal"
+          variant="filled"
+          className="mui-input"
+          sx={{
+            margin: '0px',
+            marginTop: '5px',
+          }}
+        />
+        <TextField
+          label="Email"
+          fullWidth
+          autocomplete="none"
+          variant="filled"
+          className="mui-input"
+          sx={{
+            margin: '0px',
+            marginTop: '5px',
+          }}
+        />
+        <TextField
+          label="Message"
+          fullWidth
+          multiline
+          rows={4}
+          autocomplete="none"
+          variant="filled"
+          className="mui-input"
+          sx={{
+            margin: '0px',
+            marginTop: '5px',
+          }}
+        />
+        <NativeSelect
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          defaultValue="default"
+          label="Role"
+          // onChange={handleChange}
+          fullWidth
+          sx={{
+            margin: '0px',
+            marginTop: '5px',
+          }}
+          variant="filled"
+        >
+          <option value="default" disabled>
+            Select your role
+          </option>
+          <option value="manager">Engineering Manager</option>
+          <option value="fullstack">Full Stack Engineer</option>
+          <option value="frontend">Front-End Engineer</option>
+          <option value="cloud">Cloud Engineer</option>
+          <option value="fullstack">Full Stack Engineer</option>
+          <option value="frontend">Front-End Engineer</option>
+          <option value="cloud">Cloud Engineer</option>
+          <option value="instructor">Technical Instructor</option>
+          <option value="student">Student</option>
+        </NativeSelect>
+
+        <input
+          accept="image/*"
+          style={{ display: 'none' }}
+          id="raised-button-file"
+          multiple
+          type="file"
+        />
+        <label htmlFor="raised-button-file">
+          <Button variant="raised" component="span" fullWidth>
+            Upload Here
+          </Button>
+        </label>
+
+        <Button
+          type="submit"
+          variant="contained"
+          style={{
+            borderBottomLeftRadius: '4px',
+            borderBottomRightRadius: '4px',
+          }}
+        >
+          Send
+        </Button>
+      </FormControl>
     </main>
   );
 };
