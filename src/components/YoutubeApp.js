57746295b147b9ea71a6a3aaea5063e649b52b28
JoseBorbon <joserborbon@gmail.com>
2021-12-12 22:02:18 -1000

    local storage for currBGColor

--- src/components/YoutubeApp.js
+++ src/components/YoutubeApp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from './common/Nav';
 import Routings from './Routings';
 import Footer from './common/Footer';
@@ -6,8 +6,16 @@ import Footer from './common/Footer';
 const YoutubeApp = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-  const [currBGColor, setCurrBGColor] = useState('white');
+  const [currBGColor, setCurrBGColor] = useState(() => {
+    const saved = localStorage.getItem('currBGColor');
+    const initialValue = JSON.parse(saved);
+    return initialValue || 'white';
+  });
   const [isColorSwitchToggled, setIsColorSwitchToggled] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem('currBGColor', JSON.stringify(currBGColor));
+  }, [currBGColor]);
 
   const getSearch = () => {
     if (!searchTerm) return;
